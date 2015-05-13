5d1b8efbb74e4c26d6a47d2bb6da19176322644d
SebDuret <sebastienduret@msn.com>
2015-05-13 11:20:16 +0200

    Init packages

---
+++ src/model/algo/AlgoA.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.algo;
+
+/**
+ *
+ * @author Sébastien
+ */
+public class AlgoA {
+    
+}
---
+++ src/model/robot/IRobot.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.robot;
+
+/**
+ *
+ * @author Sébastien
+ */
+public class IRobot {
+    
+}
