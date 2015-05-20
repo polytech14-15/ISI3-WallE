496e50dbb4477dbe31509a39118d70da299709b2
SebDuret <sebastienduret@msn.com>
2015-05-20 11:23:08 +0200

    fix duplicate method

--- src/model/graph/Graph.java
+++ src/model/graph/Graph.java
@@ -22,16 +22,8 @@ public class Graph {
     public ArrayList<Edge> getEdges() {
         return edges;
     }
-
-    public ArrayList<Node> getNodes() {
-        return nodes;
-    }
-
-    public ArrayList<Edge> getEdges() {
-        return edges;
-    }
     
-        /**
+    /**
      * @param nodes the nodes to set
      */
     public void setNodes(ArrayList<Node> nodes) {
